cc7f4399fa78ffbfe7a60ec7be6a8e1e356b841d
Alaa Qutaish <alaa.qutaish@gmail.com>
2013-10-27 15:34:11 +0100

    add workaholic class

---
+++ workaholic.rb
@@ -0,0 +1,28 @@
+#!/bin/ruby
+require 'time'
+
+class Workaholic
+  attr_accessor :proccess, :limit
+
+  def initialize proccess, limit
+    @proccess = proccess
+    @limit = limit.to_i
+  end
+
+  def scan
+    # Get all child processes of the desired parent process.
+    pids = %x(ps -o pid,etime --ppid=`pidof -s #{proccess}` --sort=etime | awk '{print $1,$2}' | tr '-' ' ' | awk '{print $1,$3}' | egrep '^[0-9]' | grep ':')
+    workaholics = {}
+    pids.each_line do |proccess|
+      pid, etime = proccess.split
+      workaholics[pid.to_sym] = etime_to_i etime if (etime_to_i etime) > limit
+    end
+    workaholics
+  end
+
+  def etime_to_i etime
+    h,m,s = etime.split(':')
+    (h.to_i * 60 * 60) + (m.to_i * 60) + (s.to_i) 
+  end
+end
+
